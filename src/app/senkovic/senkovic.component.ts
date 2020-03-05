c41766a324034bbf2ede8c9a6fff582727424727
angular-edukacije <vedran.prpic@gmail.com>
2020-03-05 15:56:52 +0100

    senkovic 2

--- src/app/senkovic/senkovic.component.ts
+++ src/app/senkovic/senkovic.component.ts
@@ -9,10 +9,46 @@ export class SenkovicComponent implements OnInit {
 
 
   nekaVarijabla = "senkovic";
-  
+  lista: any = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  callService(){
+    this.nekaVarijabla = "senkovic";
+    var url = "https://jsonplaceholder.typicode.com/posts";
+    this.getData(url).then(data => {
+      console.log("Ovo je vratio weather: ", data);
+      if(data){
+        this.nekaVarijabla = "uspješan dohvat podataka";
+        this.lista = data["Response"];
+      }
+      else{
+        this.nekaVarijabla = "Nema podataka :(";
+      }
+    });
+  }
+
+
+  getData(url): Promise<Response> {
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });    
+  }
+
+  getWeather(): Promise<Response> {
+    const url = "https://localhost:44385/weatherforecast";
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
   }
 
 }
