0053475d68cc32cd293857c3a899db5a393ead97
angular-edukacije <vedran.prpic@gmail.com>
2020-03-10 15:51:51 +0100

    miroslav

--- src/app/miroslav/miroslav.component.ts
+++ src/app/miroslav/miroslav.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./miroslav.component.css']
 })
 export class MiroslavComponent implements OnInit {
+  
+  moja='Moja varijabla';
+  drugavar='Druga varijabla';
+  mojabool = true;
+  polje=['Prvo','Drugo','Treće','Četvrto','Peto','Šest','Sedmo','Osmo','Deveto'];
 
   constructor() { }
 
   ngOnInit(): void {
+  
   }
 
+  callService()
+  {
+      console.log("Kliknio si!");
+  }
+
+  promjeni()
+  {
+    this.mojabool=!this.mojabool;
+  }
+
+  odaberi(odabrano)
+  {
+    console.log(odabrano);
+    this.polje.splice(1,1,odabrano);
+  }
 }
